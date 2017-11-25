b50c6fd542388f519d701b860d6129da68a3d235
coon <selikhovleonid@ya.ru>
2017-11-25 19:04:12 +0300

    The layout has been updated. Task #1 - Update the template codebase from php 5.3 to php 7.1.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html>
-
     <head>
         <meta charset="utf-8">
-        <title>Nadir Framework</title>
+        <title>Nadir2 Microframework</title>
     </head>
     <body>
         <?php $this->getView()->render(); ?>
